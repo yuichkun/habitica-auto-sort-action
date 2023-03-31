3f27695251494da47329506b44dd1f38bfb34b32
yuichkun <yuichkun@gmail.com>
2023-03-31 19:41:56 +0900

    add sort function

---
+++ src/date.ts
@@ -0,0 +1,13 @@
+import { HabiticaTask } from "./types";
+
+export function sortByDate(a: HabiticaTask, b: HabiticaTask) {
+  if (a.date && b.date) {
+    return new Date(a.date).getTime() - new Date(b.date).getTime();
+  } else if (a.date) {
+    return -1;
+  } else if (b.date) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
